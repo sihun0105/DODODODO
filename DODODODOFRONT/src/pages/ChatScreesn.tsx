2ecf940377ad4f3e15eda421bd06ff6c17673491
sihun <thrchtlgns@naver.com>
2022-11-03 20:56:02 +0900

    [feat] create chatscreen

---
+++ DODODODOFRONT/src/pages/ChatScreesn.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const ChatScreesn = () => {
+  return (
+    <View>
+      <Text>ChatScreesn</Text>
+    </View>
+  );
+};
+
+export default ChatScreesn;
