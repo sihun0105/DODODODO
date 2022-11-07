10dff83c35bc8c5bb47da971c918475a6eb94f6e
sihun <thrchtlgns@naver.com>
2022-11-07 15:01:47 +0900

    [feat] mypage design

--- DODODODOFRONT/src/pages/MyPage.tsx
+++ DODODODOFRONT/src/pages/MyPage.tsx
@@ -1,4 +1,4 @@
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
 import React from 'react';
 import {useSelector} from 'react-redux';
 import {RootState} from '../store/reducer';
@@ -11,9 +11,10 @@ const MyPage = ({navigation}: SignInScreenProps) => {
   console.log(userinfo);
   return (
     <SafeAreaView>
-      <ProfileTopArea email={userinfo.email} id={+userinfo.id} />
+      <ProfileTopArea email={userinfo.email} userImg={''} />
     </SafeAreaView>
   );
 };
 
 export default MyPage;
+const styles = StyleSheet.create({});
